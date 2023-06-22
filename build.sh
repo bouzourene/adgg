2e41a6f777148890ccc37a2b9581410e6326449c
William Maillard <william@zeero.ch>
2023-06-22 07:34:12 +0200

    Add .exe to Windows build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir -p ./build
 
-env GOOS=windows GOARCH=amd64 go build -o build/adgg-win-amd64
+env GOOS=windows GOARCH=amd64 go build -o build/adgg-win-amd64.exe
 env GOOS=darwin GOARCH=amd64 go build -o build/adgg-osx-amd64
 env GOOS=darwin GOARCH=arm64 go build -o build/adgg-osx-arm64
 env GOOS=linux GOARCH=amd64 go build -o build/adgg-lin-amd64
